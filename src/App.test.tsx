371e6d8c15c000dcadaf509d2e27a33247cabf27
vasser <serhiy.vas@gmail.com>
2024-09-16 18:49:45 -0400

    Fix react test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders refresh the image link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Refresh the image/i);
   expect(linkElement).toBeInTheDocument();
 });
